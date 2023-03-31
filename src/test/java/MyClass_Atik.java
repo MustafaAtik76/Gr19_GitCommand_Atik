bc25a044dbe97408b5b27ed98fe6706cbce0636e
Mustafa Atik <mustafaatik76@gmail.com>
2023-03-31 17:39:29 +0300

    my second commit

--- src/test/java/MyClass_Atik.java
+++ src/test/java/MyClass_Atik.java
@@ -2,5 +2,10 @@ public class MyClass_Atik {
 
     public static void main(String[] args) {
         System.out.println("My first work is done on local");
+
+
+        System.out.println("My second work is done on local");
+
+
     }
 }
